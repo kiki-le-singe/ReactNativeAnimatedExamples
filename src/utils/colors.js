22b515bdf71481ad3e75402326de2cbc2eb8a1ed
anthony <aalbertini95@gmail.com>
2020-02-14 17:18:12 +0100

    first commit

---
+++ src/utils/colors.js
@@ -0,0 +1,6 @@
+export default {
+    white: '#FFFFFF',
+    transparent: 'transparent',
+    blackOpacity: 'rgba(0,0,0,0.4)',
+    grey: '#D4D4D4',
+};
---
+++ src/utils/units.js
@@ -0,0 +1,13 @@
+import { Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const _vw = width / 100;
+const _vh = height / 100;
+
+export const vh = value => {
+    return value * _vh;
+};
+export const vw = value => {
+    return value * _vw;
+};
