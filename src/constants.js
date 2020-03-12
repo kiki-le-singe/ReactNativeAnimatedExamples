2913b0ce9d94248be021077dc97fa10a848dc1ba
anthony <aalbertini95@gmail.com>
2020-03-12 17:43:43 +0100

    remove unused constants

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,5 @@ import { vh } from './utils/units';
 
 export const STATUS_BAR_HEIGHT = getStatusBarHeight();
 export const HEADER_HEIGHT = 60 + STATUS_BAR_HEIGHT;
-export const HERO_HEIGHT = vh(40);
-export const TOTAL_HEIGHT = HEADER_HEIGHT + HERO_HEIGHT;
 export const INPUT_CONTAINER_HEIGHT = 40;
 export const HEADER_BACKGROUND_HEIGHT = vh(50);
