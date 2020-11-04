8171d4d3ef1392b9d086fa3f34539481d19c5727
anthony <aalbertini95@gmail.com>
2020-11-04 17:29:18 +0100

    upgrade RN

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-require('react-native').unstable_enableLogBox();
-
 AppRegistry.registerComponent(appName, () => App);
